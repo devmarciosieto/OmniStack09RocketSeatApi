46073b0da85573aff3668abfe19f6dae985055c9
Márcio Rogério Sieto <mmmsieto@gmail.com>
2019-10-02 01:34:25 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack01:omnistack01@omnistack-ciapo.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// GET, POST, PUT, DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação e edição)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
